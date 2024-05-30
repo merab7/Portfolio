4559c63a5ab3c16a900f950d639f1c24159a4208
Merab <123260233+merab7@users.noreply.github.com>
2024-05-30 23:29:42 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,37 +2,35 @@ import { FaLinkedin, FaGithubSquare } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 import picture from './image/merab.jpg';
 import TechSkills from "./TechSkills";
+
 function Hero() {
   return (
-    
-    <section className="mt-60 flex flex-row justify-center items-center   "  id="home-section" >
-      <div className="flex flex-col lg:mr-16 ">
-        <div className="w-min flex pl-5">
+    <section className="mt-16 flex flex-col lg:flex-row justify-center items-center lg:mt-60" id="home-section">
+      <div className="flex flex-col items-center lg:items-start lg:mr-16">
+        <div className="flex items-center justify-center lg:justify-start w-full pl-5">
           <h1 className="text-4xl font-bold md:text-5xl lg:text-7xl">Python Developer</h1>
-          <div className="-ml-16 md:-ml-28 lg:-ml-40 w-min text-4xl md:text-6xl lg:text-7xl animate-pulse">🌐</div>
+          <div className="ml-2 md:ml-4 lg:ml-8 text-4xl md:text-6xl lg:text-7xl animate-pulse">🌐</div>
         </div>
-        <div className="w-72 md:w-80 lg:w-86 mt-9 pl-5">
+        <div className="mt-6 w-72 md:w-80 lg:w-96 pl-5 text-center lg:text-left">
           <p>
             I'm a Python developer <span className="font-bold">Merab Todua</span> based in <span className="font-bold">Tbilisi, Georgia</span> 📍.
             I love coding.
           </p>
-          <div className="flex gap-4 transition-all">
-            <Link to="https://www.linkedin.com/in/merab-todua-682965254/" target="_blank" rel="noopener noreferrer" >
-            <FaLinkedin className="mt-5 text-4xl cursor-pointer hover-effect" />
+          <div className="flex justify-center lg:justify-start gap-4 mt-5">
+            <Link to="https://www.linkedin.com/in/merab-todua-682965254/" target="_blank" rel="noopener noreferrer">
+              <FaLinkedin className="text-4xl cursor-pointer hover-effect" />
             </Link>
             <Link to="https://github.com/topics/sun?l=css" target="_blank" rel="noopener noreferrer">
-              <FaGithubSquare className="mt-5 text-4xl cursor-pointer hover-effect" />
+              <FaGithubSquare className="text-4xl cursor-pointer hover-effect" />
             </Link>
           </div>
-
-          <div className="w-max mt-16 ">
-             <TechSkills/>
+          <div className="mt-10 lg:mt-16">
+            <TechSkills />
           </div>
         </div>
       </div>
-
-      <div className="-mt-24 w-min animate-pulse ml-0 ">
-        <div className="w-24 mask mask-hexagon md:w-28 lg:w-52 ">
+      <div className="mt-10 lg:mt-0 lg:ml-10 animate-pulse flex justify-center lg:block">
+        <div className="w-24 mask mask-hexagon md:w-28 lg:w-52">
           <img src={picture} alt="Profile Picture" />
         </div>
       </div>
