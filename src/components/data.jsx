fa814e0776d544d3714d229c0ef4db49c2e66e0e
Merab <123260233+merab7@users.noreply.github.com>
2024-05-31 00:50:13 +0200

    Update data.jsx

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -11,34 +11,34 @@ import ecom9 from "./appic/ecom9.png";
 const projectsData = [
   {
     name: "Cryptico",
-    info: `E-Commerce Site for Football Jerseys
-Project Overview:
+    info: `**E-Commerce Site for Football Jerseys**
+
+**Project Overview:**
 Developed a fully functional e-commerce site using Django, specializing in football jerseys. The site features robust user authentication, profile management, and admin capabilities.
 
-Key Features:
+**Key Features:**
 
-User Experience:
+**User Experience:**
+- Browse & Purchase: Users can explore and buy jerseys as guests or registered users.
+- Secure Authentication: Implemented Django-based system with email verification.
+- Profile Management: Users can view order history, track orders, and update shipping/billing info.
 
-Browse & Purchase: Users can explore and buy jerseys as guests or registered users.
-Secure Authentication: Implemented Django-based system with email verification.
-Profile Management: Users can view order history, track orders, and update shipping/billing info.
-Admin Capabilities:
+**Admin Capabilities:**
+- Product Management: Admins can easily upload and manage products, sizes, and pricing using Django models.
 
-Product Management: Admins can easily upload and manage products, sizes, and pricing using Django models.
-Order Processing:
+**Order Processing:**
+- Seamless Checkout: Streamlined process with email confirmations.
+- Order Notifications: Users receive email updates on order status.
 
-Seamless Checkout: Streamlined process with email confirmations.
-Order Notifications: Users receive email updates on order status.
-Technologies Used:
+**Technologies Used:**
+- Backend: Django (authentication, email integration)
+- Frontend: HTML, CSS, JavaScript
+- Database Management: Handled via Django models
 
-Backend: Django (authentication, email integration)
-Frontend: HTML, CSS, JavaScript
-Database Management: Handled via Django models
-Achievements:
-
-Built a secure, user-friendly e-commerce platform from scratch.
-Developed an intuitive admin interface for efficient product management.
-Implemented a complete order processing system with real-time email notifications.`,
+**Achievements:**
+- Built a secure, user-friendly e-commerce platform from scratch.
+- Developed an intuitive admin interface for efficient product management.
+- Implemented a complete order processing system with real-time email notifications.`,,
     photos: {
       pictone: ecom1,
       picttwo: ecom2,
