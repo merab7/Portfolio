a45403da242204de71c9eaef6405ff263bfa55a5
Merab <123260233+merab7@users.noreply.github.com>
2024-10-24 20:31:04 +0400

    Update Aboutme.jsx

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -11,10 +11,9 @@ function Aboutme() {
         </h2>
 
         <p className="pt-5 leading-relaxed">
-       My name is Merab Todua, and I am a Python developer with a passion for web development and data analysis. I enjoy working in this field and continually strive to enhance my skills to stay updated with the latest advancements in technology. One of my proudest accomplishments is building <span><Link to="https://www.jerseys.ge/" target="_blank" rel="noopener noreferrer">
-       jerseys.ge</Link></span> a fully functional e-commerce platform developed from scratch for a real business. This project demonstrates my ability to create dynamic, user-driven websites with a focus on functionality, scalability, and user experience.
-
-I am confident that my ongoing dedication to learning and growth will lead to continued success in this ever-evolving industry. If we work together, you can count on me to see projects through to the end while maintaining high standards. I welcome the opportunity to connect and collaborate on exciting projects!
+       My name is Merab Todua, and I am a Python developer with a passion for web development and data analysis. I enjoy working in this field and continually strive to enhance my skills to stay updated with the latest advancements in technology. One of my proudest accomplishments is building 
+       jerseys.ge a fully functional e-commerce platform developed from scratch for a real business. This project demonstrates my ability to create dynamic, user-driven websites with a focus on functionality, scalability, and user experience.
+       I am confident that my ongoing dedication to learning and growth will lead to continued success in this ever-evolving industry. If we work together, you can count on me to see projects through to the end while maintaining high standards. I welcome the opportunity to connect and collaborate on exciting projects!
         </p>
       </div>
     </section>
